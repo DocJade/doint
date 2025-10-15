c4f598905a61c98a8f5b892560b56bd2bb4b507b
DocJade <ytdocjade@gmail.com>
2025-10-15 06:31:40 -0600

    Wrong text on stealing implied that the person you robbed stole from you

--- src/invocable/standard/crime/rob.rs
+++ src/invocable/standard/crime/rob.rs
@@ -206,7 +206,7 @@ const SUCCESS_FLAVOR: [&str; 5] = [
     "You ran by and stole *'s hat worth",
     "You ran off with",
     "You tied *'s shoelaces together while you leafed through their wallet, taking",
-    "You pointed a banana at *, and they thought it was a gun! * Ran off with",
+    "You pointed a banana at *, and they thought it was a gun! You Ran off with",
     "You dipped into *'s back pocket, which contained 3 jelly beans, a fish skeleton, and",
 ];
 
